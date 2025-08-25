1633cbdd0cf1a3427bc2e4c92d945f170d93ba6c
Magnus Lidström <malstrom72@gmail.com>
2025-08-25 14:40:33 +0200

    use tabs in source viewer block

--- tools/testdash.node.js
+++ tools/testdash.node.js
@@ -124,8 +124,9 @@ var server = http.createServer( function(req, res) {
 		if (u.pathname.substr(0,8) === "/source/") {
 			var src = u.pathname.substr(8);
 			if (!src.endsWith(".js")) src += ".js";
+			if (!src.startsWith("test/")) src = "test/" + src;
 			console.log("Source request: " + src);
-			u.pathname = "/" + TEST_PATH + "test/" + src;
+			u.pathname = "/" + TEST_PATH + src;
 			isSource = true;
 		}
 
