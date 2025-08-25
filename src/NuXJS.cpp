b3b0ecb365efab9c68cf63b146da7e9e1a0517fb
Magnus Lidström <malstrom72@gmail.com>
2025-08-25 15:55:04 +0200

    Optimize string constant lookup

--- src/NuXJS.cpp
+++ src/NuXJS.cpp
@@ -5006,28 +5006,17 @@ Var Runtime::newObjectVar() { return Var(*this, newJSObject()); }
 Var Runtime::newArrayVar(UInt32 initialLength) { return Var(*this, newJSArray(initialLength)); }
 
 const String* Runtime::newStringConstantWithHash(UInt32 hash, const char* s) {
-	const String* cached = stringConstantsCache[hash];
-	size_t length;
-	if (cached != 0) {
-		const char* p0 = s;
-		const Char* p1 = cached->begin();
-		const Char* e = cached->end();
-		while (*p0 != 0 && p1 != e && *p0 == *p1) {
-			++p0;
-			++p1;
-		}
-		if (*p0 == 0 && p1 == e) {
-			return cached;
-		}
-		length = p0 - s + strlen(p0);
-	} else {
-		length = strlen(s);
-	}
-	const String* string = new(heap) String(heap.managed(), s, s + length);
-	if (length < STRING_CONSTANTS_CACHE_MAX_LENGTH) {
-		stringConstantsCache[hash] = string;
-	}
-	return string;
+       const String* cached = stringConstantsCache[hash];
+       const size_t length = std::strlen(s);
+       if (cached != 0 && cached->size() == length
+                       && std::equal(cached->begin(), cached->end(), s)) {
+               return cached;
+       }
+       const String* string = new(heap) String(heap.managed(), s, s + length);
+       if (length < STRING_CONSTANTS_CACHE_MAX_LENGTH) {
+               stringConstantsCache[hash] = string;
+       }
+       return string;
 }
 
 Var Runtime::call(Function* function, UInt32 argc, const Value* argv, Object* thisObject) {
