e750cc7d4a049c46e946772bcb406006c4df1197
Magnus Lidström <malstrom72@gmail.com>
2025-08-26 15:04:41 +0200

    Optimize deleteAll sweep

--- src/NuXJS.cpp
+++ src/NuXJS.cpp
@@ -1147,11 +1147,13 @@ void GCList::claim(GCItem* item) throw() {
 }
 
 void GCList::deleteAll() throw() {
-	for (const GCItem* i = _gcNext->_gcNext; i != _gcNext; i = i->_gcNext) {
-		assert(i->_gcPrev->_gcList == this);
-		delete i->_gcPrev;
-	}
-	assert(_gcPrev == _gcNext && _gcPrev == this);
+       GCItem* i = _gcNext;
+       while (i != this) {
+               GCItem* next = i->_gcNext;
+               delete i;
+               i = next;
+       }
+       assert(_gcPrev == _gcNext && _gcPrev == this);
 }
 
 /* --- Heap --- */
