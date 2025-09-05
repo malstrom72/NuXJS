e9858a5335992c83e23b873ee1b5f368a81a3856
Magnus Lidström <malstrom72@gmail.com>
2025-09-05 16:06:35 +0200

    Add regression test for Object.prototype.toString undefined/null

--- src/stdlib.js
+++ src/stdlib.js
@@ -223,6 +223,8 @@ defineProperties(Object.prototype, { dontEnum: true }, {
 	valueOf: unconstructable(function valueOf() { return this }),
 	toLocaleString: unconstructable(function toLocaleString() { return this.toString() }),
 	toString: unconstructable(function toString() {
+			if (this === void 0) return "[object Undefined]";
+			if (this === null) return "[object Null]";
 		var s;
 		return "[object " + (((s = $getInternalProperty(this, "class")) === "Arguments") ? "Object" : s) + ']'
 	}),
@@ -811,11 +813,11 @@ function dateFromEpoch(z) {
 	var doe = z - era * 146097;
 	var yoe = int( (doe - int(doe / 1460) + int(doe / 36524) - int(doe / 146096)) / 365 );
 	var y = yoe + era * 400;
-    var doy = doe - (365 * yoe + int(yoe / 4) - int(yoe/100) );
-    var mp = int( (5 * doy + 2) / 153);
-    var m = mp + (mp < 10 ? 2 : -10);
-    var d = doy - int( (153 * mp + 2) / 5 ) + 1;
-    return [ (y + (m <= 1)), m, d ];
+	var doy = doe - (365 * yoe + int(yoe / 4) - int(yoe/100) );
+	var mp = int( (5 * doy + 2) / 153);
+	var m = mp + (mp < 10 ? 2 : -10);
+	var d = doy - int( (153 * mp + 2) / 5 ) + 1;
+	return [ (y + (m <= 1)), m, d ];
 }
 
 function epochToDateString(z) {
@@ -899,7 +901,7 @@ defineProperties(Date, { dontEnum: true }, {
 			++i, tzh = readPart(2) * 36e5,
 			s[i] === ":" && ++i, tzh += $isNaN(tzm = readPart(2)) ? 0 : tzm * 6e4,
 			$isNaN(tzh) || (tz = ch === "-" ? -tzh : tzh);
-	    }
+		}
 		return (tz === void 0 ? fromLocalTime(z) : z - tz)
 	}),
 	UTC: unconstructable(function UTC(year, month, date, hours, minutes, seconds, ms) { 
@@ -1657,12 +1659,12 @@ defineProperties(JSON, { dontEnum: true }, {
 			for (var i = replacer.length; --i >= 0;) includeProps[replacer[i]] = true;
 		}
 
-        if (typeof space === "number" || (typeof space === "object" && $getInternalProperty(space, "class") === "Number")) {
-        	space = +space;
-            for (var i = (space > 10 ? 10 : space); --i >= 0;) gap += ' ';
-        } else if (typeof space === "string" || (typeof space === "object" && $getInternalProperty(space, "class") === "String")) {
-            gap = $sub(str(space), 0, 10);
-        }
+		if (typeof space === "number" || (typeof space === "object" && $getInternalProperty(space, "class") === "Number")) {
+			space = +space;
+			for (var i = (space > 10 ? 10 : space); --i >= 0;) gap += ' ';
+		} else if (typeof space === "string" || (typeof space === "object" && $getInternalProperty(space, "class") === "String")) {
+			gap = $sub(str(space), 0, 10);
+		}
 
 		function quote(s) {
 			var t = '"', len = s.length;
@@ -1677,17 +1679,17 @@ defineProperties(JSON, { dontEnum: true }, {
 
 		function string(key, holder, indent) {
 			var val;
-	        if ((val = holder[key]) && typeof val === "object" && typeof val.toJSON === "function") val = val.toJSON(key);
-	        if (replacerFunction) val = $callWithArgs(replacerFunction, holder, [ key, val ]);
+			if ((val = holder[key]) && typeof val === "object" && typeof val.toJSON === "function") val = val.toJSON(key);
+			if (replacerFunction) val = $callWithArgs(replacerFunction, holder, [ key, val ]);
 
-	        var lineEnd = (gap ? '\n' + indent : '');
-	        if (typeof val === "object") {
-	        	switch ($getInternalProperty(val, "class")) {
-	        		case "Number": val = +val; break;
-	        		case "String": val = str(val); break;
-	        		case "Boolean": val = $getInternalProperty(val, "value"); break;
-	        	}
-	        }
+			var lineEnd = (gap ? '\n' + indent : '');
+			if (typeof val === "object") {
+				switch ($getInternalProperty(val, "class")) {
+					case "Number": val = +val; break;
+					case "String": val = str(val); break;
+					case "Boolean": val = $getInternalProperty(val, "value"); break;
+				}
+			}
 			switch (typeof val) {
 				case "object": {
 					if (!val) return "null";
@@ -1811,19 +1813,19 @@ defineProperties(JSON, { dontEnum: true }, {
 		if ((parser = PARSERS[text[p = space(text, 0)]]) && (p = parser(text, p))
 				&& space(text, p) === text.length) {
 			var val = eval('(' + text + ')');
-            if (typeof reviver === "function") {
-            	function walk(holder, key) {
-	                var k, v, o;
-	                if (typeof (o = holder[key]) === "object" && o) {
-	                    for (k in o) {
-	                        if (support.hasOwnProperty(o, k)) {
-	                            if ((v = walk(o, k)) !== void 0) o[k] = v;
-	                            else delete o[k];
-	                        }
-	                    }
-	                }
-	                return $callWithArgs(reviver, holder, [ key, o ]);
-	            }
+			if (typeof reviver === "function") {
+				function walk(holder, key) {
+					var k, v, o;
+					if (typeof (o = holder[key]) === "object" && o) {
+						for (k in o) {
+							if (support.hasOwnProperty(o, k)) {
+								if ((v = walk(o, k)) !== void 0) o[k] = v;
+								else delete o[k];
+							}
+						}
+					}
+					return $callWithArgs(reviver, holder, [ key, o ]);
+				}
 				val = walk({ "": val }, "");
 			}
 			return val;
---
+++ tests/regression/objectPrototypeToStringUndefinedNull.io
@@ -0,0 +1,7 @@
+> print(Object.prototype.toString.call(undefined))
+< [object Undefined]
+-
+> print(Object.prototype.toString.call(null))
+< [object Null]
+-
+
