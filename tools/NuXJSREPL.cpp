ccce74d93f73cd00c22adc9ddd90b9dbbbc57d51
Magnus Lidström <malstrom72@gmail.com>
2025-09-24 23:04:44 +0200

    Add legacy exception output toggle to NuXJS REPL

--- tools/NuXJSREPL.cpp
+++ tools/NuXJSREPL.cpp
@@ -471,11 +471,13 @@ int testMain(int argc, const char* argv[]) {
 		size_t peakMemory = 0;
 		bool doSuppressStdErr = false;
 		bool loadStdLib = true;
+		bool useLegacyExceptionOutput = false;
 		for (int argi = 1; argi < argc; ++argi) {
 			if (strcmp(argv[argi], "-t") == 0) doTime = true;
 			else if (strcmp(argv[argi], "-s") == 0) doSuppressStdErr = true;
 			else if (strcmp(argv[argi], "-p") == 0) pauseBeforeQuit = true;
 			else if (strcmp(argv[argi], "-n") == 0) loadStdLib = false;
+			else if (strcmp(argv[argi], "--legacy-exceptions") == 0 || strcmp(argv[argi], "-E") == 0) useLegacyExceptionOutput = true;
 			else if (inputFilePath.empty()) {
 				inputFilePath = argv[argi];
 				interactive = false;
@@ -492,6 +494,7 @@ int testMain(int argc, const char* argv[]) {
 
 		const String LF_STRING("\n");
 		const String PRINT_STRING("print");
+		static const String PRINT_EXCEPTION_METADATA_STRING("__printExceptionMetadata__");
 
 		MyHeap heap;
 		Runtime rt(heap);
@@ -616,6 +619,13 @@ int testMain(int argc, const char* argv[]) {
 				
 				if (execute) {
 					Code globalCode(heap.roots());
+					const String* scriptFileName = 0;
+					if (!inputFilePath.empty()) {
+							scriptFileName = String::allocate(heap, inputFilePath.c_str());
+					} else {
+							scriptFileName = rt.newStringConstant("<anonymous>");
+					}
+					globalCode.setFileName(scriptFileName);
 					Compiler compiler(heap.roots(), &globalCode, (interactive ? Compiler::FOR_EVAL : Compiler::FOR_GLOBAL));
 					try {
 						compiler.compile(source);
@@ -665,18 +675,57 @@ int testMain(int argc, const char* argv[]) {
 					source = EMPTY_STRING;
 				}
 			}
-			catch (const ScriptException& x) {
-				source = EMPTY_STRING;
-				std::wstring ws = x.value.toString(heap)->toWideString();
-				ws = L"!!!! " + ws;
-				std::wcout << ws << std::endl;
-				if (!interactive) {
-					return 1;
-				} else {
-					pushIOLines('!', String(heap.roots(), ws.c_str()));
+		catch (const ScriptException& x) {
+			source = EMPTY_STRING;
+			std::wstring ws = x.value.toString(heap)->toWideString();
+			ws = L"!!!! " + ws;
+			std::wcout << ws << std::endl;
+			bool printMetadata = !useLegacyExceptionOutput;
+			if (!useLegacyExceptionOutput) {
+				Value metadataFlag(Value::UNDEFINED);
+				if (rt.getGlobalObject()->getProperty(rt, Value(&PRINT_EXCEPTION_METADATA_STRING), &metadataFlag) != NONEXISTENT) {
+					printMetadata = metadataFlag.toBool();
 				}
 			}
-			catch (const std::exception& x) {
+			std::string locationLine;
+			std::string stackLine;
+			if (printMetadata) {
+				std::ostringstream locationStream;
+				const String* fileName = x.getFileName();
+				if (fileName != 0) {
+					locationStream << fileName->toUTF8String();
+				} else {
+					locationStream << "<anonymous>";
+				}
+				if (x.getLineNumber() > 0) {
+					locationStream << ':' << x.getLineNumber();
+					if (x.getColumnNumber() > 0) {
+						locationStream << ':' << x.getColumnNumber();
+					}
+				}
+				locationLine = std::string("!!!! location: ") + locationStream.str();
+				const std::wstring locationWide(locationLine.begin(), locationLine.end());
+				std::wcout << locationWide << std::endl;
+				const char* formattedStack = x.formatStackTrace();
+				if (formattedStack != 0 && formattedStack[0] != '\0') {
+					stackLine = std::string("!!!! stack: ") + formattedStack;
+					const std::wstring stackWide(stackLine.begin(), stackLine.end());
+					std::wcout << stackWide << std::endl;
+				}
+			}
+			if (!interactive) {
+				return 1;
+			} else {
+				pushIOLines('!', String(heap.roots(), ws.c_str()));
+				if (printMetadata && !locationLine.empty()) {
+					pushIOLines('!', String(heap.roots(), locationLine.c_str()));
+				}
+				if (printMetadata && !stackLine.empty()) {
+					pushIOLines('!', String(heap.roots(), stackLine.c_str()));
+				}
+			}
+		}
+		catch (const std::exception& x) {
 				source = EMPTY_STRING;
 				std::cout << "!!!! " << x.what() << std::endl;
 				if (!interactive) {
--- tools/buildAndTest.sh
+++ tools/buildAndTest.sh
@@ -22,7 +22,7 @@ bash ./BuildCpp.sh $target $model ../output/NuXJSTest_${target}_${model} ../tool
 ../output/NuXJSTest_${target}_${model} -s >/dev/null 2>&1
 ../output/NuXJSTest_${target}_${model}
 bash ./BuildCpp.sh $target $model ../output/NuXJS_${target}_${model} ../tools/NuXJSREPL.cpp ../src/NuXJS.cpp ../src/stdlibJS.cpp
-../externals/PikaCmd/PikaCmd ./test.pika -e -x ../output/NuXJS_${target}_${model} ../tests/
+../externals/PikaCmd/PikaCmd ./test.pika -e -x "../output/NuXJS_${target}_${model} -s --legacy-exceptions" ../tests/
 bash ./runExamples.sh "$target"
 
 echo Success!
